c5c5e9a59457bffa2720674250bf7fc8c3889bf2
Batuhan Inan <Batuhan.Inan@softtech.com.tr>
2023-12-02 10:55:20 +0300

    Day 2 fix

--- 23/day_1/2.py
+++ 23/day_1/2.py
@@ -18,7 +18,7 @@ def main():
     for line in data.split():
         _line = line
         for number in numbers_spelled.keys():
-            if numbers_spelled[number] in _line:
+            if number in _line:
                 _line = _line.replace(number, number[0] + numbers_spelled[number] + number[-1])
         numbers = [c for c in _line if c.isdigit()]
         result += int(numbers[0] + numbers[-1])
